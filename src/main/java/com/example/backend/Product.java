fbd2a1229288074f0694c9c2e31bcdf0f2a18c75
awryme <awryme@gmail.com>
2017-10-13 16:24:39 +0300

    Добавил Product, ProductStorage, ProductController

---
+++ src/main/java/com/example/backend/Product.java
@@ -0,0 +1,53 @@
+package com.example.backend;
+
+/**
+ * Created by Andreyko0 on 13/10/2017.
+ */
+public class Product {
+  private String name;
+  private String description;
+  private String id;
+
+  public Product(String name) {
+    this.name = name;
+  }
+
+  public Product(String name, String desctiption) {
+    this.name = name;
+    this.description = desctiption;
+  }
+  public Product(String id, String name, String desctiption) {
+    this.id = id;
+    this.name = name;
+    this.description = desctiption;
+  }
+  public Product() {
+    this.id = "def";
+    this.name = "def";
+    this.description = "def";
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getId() {
+    return id;
+  }
+}
---
+++ src/main/java/com/example/backend/ProductController.java
@@ -0,0 +1,30 @@
+package com.example.backend;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * Created by Andreyko0 on 13/10/2017.
+ */
+@RestController
+@RequestMapping("/pr")
+public class ProductController {
+
+  @RequestMapping(value = "/new", method = RequestMethod.POST)
+  @ResponseBody
+  public void addProduct(@RequestBody Product p, HttpServletResponse resp) {
+    ProductStorage.addProduct(p);
+    System.out.println("Got prd");
+    resp.setStatus(HttpServletResponse.SC_OK);
+  }
+
+  @RequestMapping(value = "/all", method = RequestMethod.GET)
+  public Map<String, Product> getAll() {
+    return ProductStorage.storage;
+  }
+}
