7435b7b815e8ec0dd3024297052d37d5122ca067
awryme <awryme@gmail.com>
2017-10-27 03:33:52 +0300

    log

--- src/main/java/com/example/backend/EmailService.java
+++ src/main/java/com/example/backend/EmailService.java
@@ -19,12 +19,15 @@ public class EmailService {
   }
 
   public void sendMail(String email, String id) {
+    System.out.println("Building email for "+ email + "with id: "+id);
     SimpleMailMessage mailMessage = new SimpleMailMessage();
     mailMessage.setTo(email);
     mailMessage.setSubject("Confirm registration");
     // add link
     mailMessage.setText("Follow the link to finish your registraton:\n"
             +"http://localhost:8080/user/confirm/"+id);
+    System.out.println("Sending mail");
     javaMailSender.send(mailMessage);
+    System.out.println("Sent");
   }
 }
