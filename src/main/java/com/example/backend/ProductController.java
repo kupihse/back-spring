c4223280b86ade275c7d87ebd0afa9ccc2ccc082
awryme <awryme@gmail.com>
2017-10-28 01:58:34 +0300

    1

--- src/main/java/com/example/backend/ProductController.java
+++ src/main/java/com/example/backend/ProductController.java
@@ -20,6 +20,7 @@ public class ProductController {
     resp.setStatus(HttpServletResponse.SC_OK);
     // some stupid testing changes
     // hey
+    
   }
 
   @RequestMapping(value = "/all", method = RequestMethod.GET)
