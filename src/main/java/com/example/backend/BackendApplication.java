26a370280c94f6c6d644572b8fe3f4166ae4b111
awryme <awryme@gmail.com>
2017-11-06 22:24:08 +0300

    123

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -15,9 +15,9 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
-	@Bean
-	@ConfigurationProperties(prefix="spring.datasource")
-	public DataSource dataSource() {
-		return DataSourceBuilder.create().build();
-	}
+//	@Bean
+//	@ConfigurationProperties(prefix="spring.datasource")
+//	public DataSource dataSource() {
+//		return DataSourceBuilder.create().build();
+//	}
 }
