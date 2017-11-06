880f1663aa24861b0e7152575028d5f760306502
awryme <awryme@gmail.com>
2017-11-06 22:27:10 +0300

    qwe

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -2,11 +2,11 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//
+//import javax.sql.DataSource;
 
 @SpringBootApplication
 public class BackendApplication {
--- src/main/java/com/example/backend/TestController.java
+++ src/main/java/com/example/backend/TestController.java
@@ -1,15 +1,15 @@
 package com.example.backend;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.sql.DataSource;
+//import javax.sql.DataSource;
 
 /**
  * Created by Andreyko0 on 06/11/2017.
