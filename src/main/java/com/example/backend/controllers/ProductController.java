0ec0f2ee070e31aeaa481d14ae633b9c6dea973e
awryme <awryme@gmail.com>
2018-01-22 21:29:32 +0300

    and again

--- src/main/java/com/example/backend/controllers/ProductController.java
+++ src/main/java/com/example/backend/controllers/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 /**
@@ -36,7 +37,7 @@ public class ProductController {
   }
 
   @RequestMapping(value = "/id/{id}", method = RequestMethod.GET)
-  public Product getProduct(@PathVariable("id") String id) {
+  public Product getProduct(@PathParam("id") String id) {
     return storage.getProduct(id);
   }
 }
