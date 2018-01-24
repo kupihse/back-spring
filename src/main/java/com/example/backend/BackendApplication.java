17199d25656c174c9f545e7d91143341996c2b23
awryme <awryme@gmail.com>
2018-01-24 21:50:52 +0300

    + описание

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+
 @SpringBootApplication
+
+// Здесь указаны файлы конфигурации
+// pass.properties – хранится локально на сервере
 @PropertySource("classpath:application.properties")
 @PropertySource("classpath:pass.properties")
+
 public class BackendApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(BackendApplication.class, args);
