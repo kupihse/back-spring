f7f740843acf87e28d031c2c8c70b2da210db93c
awryme <awryme@gmail.com>
2018-01-24 21:44:00 +0300

    properties... again

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -2,8 +2,11 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
+@PropertySource("classpath:pass.properties")
 public class BackendApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(BackendApplication.class, args);
