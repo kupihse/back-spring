37f896daf7a1aa67e1d690c83123f259ddaa3470
awryme <awryme@gmail.com>
2018-02-05 00:48:24 +0300

    + storage в байтах и два url up/download

--- src/main/java/com/example/backend/controllers/ImageController.java
+++ src/main/java/com/example/backend/controllers/ImageController.java
@@ -1,9 +1,18 @@
 package com.example.backend.controllers;
 
 import com.example.backend.models.Image;
+import com.example.backend.storages.ImageByteMemoryStorage;
 import com.example.backend.storages.dao.ImageDAO;
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by Andreyko0 on 25/01/2018.
@@ -14,6 +23,9 @@ public class ImageController {
 
   @Autowired
   private ImageDAO storage;
+
+  @Autowired
+  private ImageByteMemoryStorage byteMemoryStorage;
 
   @PostMapping("/add")
   void addImage(@RequestBody Image img) {
@@ -29,4 +41,30 @@ public class ImageController {
   void deleteAll() {
     storage.deleteAll();
   }
+
+  @PostMapping("/images/upload/{id}")
+  void upload(@PathVariable("id") String id, HttpServletRequest request) {
+    ByteOutputStream stream = new ByteOutputStream();
+    try {
+      IOUtils.copy(request.getInputStream(), stream);
+    } catch (IOException e) {
+      System.out.println("Couldn;t upload: "+e.getMessage());
+      return;
+    }
+    byteMemoryStorage.saveImage(id, stream.getBytes());
+  }
+
+  @GetMapping("/images/download/{id}")
+  void download(@PathVariable("id") String id, HttpServletResponse response) {
+    byte[] bytes = byteMemoryStorage.getImage(id);
+    ByteInputStream stream = new ByteInputStream();
+    try {
+      stream.read(bytes);
+//      response.getOutputStream()
+      IOUtils.copy(stream, response.getOutputStream());
+    } catch (IOException e) {
+      System.out.println("Couldn;t download: "+e.getMessage());
+    }
+  }
+
 }
