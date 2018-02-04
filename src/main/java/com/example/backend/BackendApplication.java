03fda954318a3217ef1da6f89a2192d3fc8e9af8
awryme <awryme@gmail.com>
2018-02-05 01:47:55 +0300

    asd2

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+import java.io.File;
+
 
 @SpringBootApplication
 
@@ -14,6 +16,20 @@ import org.springframework.context.annotation.PropertySource;
 
 public class BackendApplication {
 	public static void main(String[] args) throws Exception {
+
+		File imDir = new File("/root/imgs");
+		if (!imDir.exists()) {
+			if (!imDir.mkdir()) {
+				System.out.println("Couldn't start");
+				return;
+			}
+		}
+
+		if (!imDir.isDirectory()) {
+			System.out.println("Couldn't start, not a directory");
+			return;
+		}
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 }
